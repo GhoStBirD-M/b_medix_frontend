1d0d467c7ef2f065352b52ab72af74bd31260609
GhoStBirD-M <ghostbird356@gmail.com>
2025-04-28 23:17:52 +0700

    first

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/usr/lib/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/ghostbird/Documents/b_medix_frontend"
+export "FLUTTER_APPLICATION_PATH=/home/ghostbird/b_medix_frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
